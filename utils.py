351c68425f770d551c56ae0c657042407465108e
ShikhaERAV2 <160948226+ShikhaERAV2@users.noreply.github.com>
2024-02-23 11:52:14 +0530

    Image Classification Model. First commit for  the Session 5 assignment

---
+++ utils.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+def display_sample_images(train_loader):
+
+  batch_data, batch_label = next(iter(train_loader))
+
+  fig = plt.figure()
+
+  for i in range(12):
+    plt.subplot(3,4,i+1)
+    plt.tight_layout()
+    plt.imshow(batch_data[i].squeeze(0), cmap='gray')
+    plt.title(batch_label[i].item())
+    plt.xticks([])
+    plt.yticks([])
+
+
+def display_accuracy(train_losses,train_acc,test_losses,test_acc):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
